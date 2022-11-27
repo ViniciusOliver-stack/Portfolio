9c89c3d6af5e0ad81d5b31babf589e82a21f30d5
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2022-11-27 15:39:15 -0300

    submit project

---
+++ assets/js/interactions.js
@@ -0,0 +1,17 @@
+LottieInteractivity.create({
+  player: '#lottie-dev',
+  mode: 'cursor',
+  actions: [
+      {
+          position: { x: [0, 1], y: [-1, 2] },
+          type: 'seek',
+          frames: [0, 480],
+      },
+      {
+          position: { x: -1, y: -1 },
+          type: 'stop',
+          frames: [0],
+      },
+  ],
+});
+
